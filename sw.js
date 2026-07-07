79e4e65a5e4c8ad5601d41c72cbbfcd2bcd5ed04
mahapatraswapnajit-prog <mahapatraswapnajit@gmail.com>
2026-07-07 09:09:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gyaanpe-v2';
+const CACHE = 'gyaanpe-v3';
 const FILES = [
   '/gyaanpe/',
   '/gyaanpe/index.html',
@@ -11,11 +11,25 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  if (e.request.mode === 'navigate' || e.request.url.indexOf('index.html') > -1) {
+    e.respondWith(
+      fetch(e.request).then(r => {
+        var cp = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, cp));
+        return r;
+      }).catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request))
+    );
+  }
 });
